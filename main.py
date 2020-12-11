b05619e2dde3e2e631e215022cefd12a51ad112c
Grafiks11 <grafiks11@gmail.com>
2020-12-11 17:35:03 +0100

    Test

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import os
 
 root = tk.Tk()
 
+def robRzeczy():
+    label=tk.Label(frame, text="ELOELOELO")
+    label.pack()
+
 canvas = tk.Canvas(root, height=700,width=700,bg="#263D42")
 canvas.pack()
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-button=tk.Button(root, text="Kliknij mnie bardzo plis",padx=20,pady=10,fg="white",bg="#123D42")
+button=tk.Button(root, text="Kliknij mnie bardzo plis",padx=20,pady=10,fg="white",bg="#123D42", command=robRzeczy)
 button.pack()
 
 button2=tk.Button(root, text="Kliknij mnie także",padx=35,pady=10,fg="white",bg="#122D42")
 button2.pack()
 
 root.mainloop()
+
+with open('save.txt','w') as f: #generates file save.txt and set write
+    f.write("treść")
